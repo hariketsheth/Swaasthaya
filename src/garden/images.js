81f4b87b35acd74abb146c755bbc436c1be57c53
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-04 01:49:44 +0530

    🎉 IMPLEMENT: Garden and GardenPage

---
+++ src/garden/images.js
@@ -0,0 +1,5 @@
+import land_both from "../garden_assets/land_both.png";
+import land_light from "../garden_assets/land_light.png";
+import land_dark from "../garden_assets/land_dark.png";
+
+export { land_both, land_light, land_dark };

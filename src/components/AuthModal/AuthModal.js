2fd2d5f7cb88881a1a761580cc4f171dcede7e5c
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-03 18:15:24 +0530

    📦 NEW: Auth Modal

---
+++ src/components/AuthModal/AuthModal.js
@@ -0,0 +1,17 @@
+import React from "react";
+import RegisterModal from "../RegisterModal/RegisterModal";
+import SignInModal from "../SignInModal/SignInModal";
+
+const AuthModal = ({ register, toggleMethod }) => {
+  return (
+    <div className="auth__modal">
+      {register ? (
+        <RegisterModal toggleMethod={toggleMethod} />
+      ) : (
+        <SignInModal toggleMethod={toggleMethod} />
+      )}
+    </div>
+  );
+};
+
+export default AuthModal;

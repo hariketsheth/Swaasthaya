835eb6821163ddc2f7e56f1010529482ece17539
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-04 01:28:53 +0530

    👌 IMPROVE: Daily habits

--- src/components/DailyHabits/DailyHabits.js
+++ src/components/DailyHabits/DailyHabits.js
@@ -4,39 +4,18 @@ import "./DailyHabits.css";
 
 const DailyHabits = () => {
   return (
-    <div className="timeline">
-      <ul>
-        <li>
-          <div className="timeline__content">
-            <HabitCard title="Tidy up bed in the morning" />
-          </div>
-        </li>
-        <li>
-          <div className="timeline__content">
-            <HabitCard title="Tidy up bed in the morning" />
-          </div>
-        </li>
-        <li>
-          <div className="timeline__content">
-            <HabitCard title="Tidy up bed in the morning" />
-          </div>
-        </li>
-        <li>
-          <div className="timeline__content">
-            <HabitCard title="Tidy up bed in the morning" />
-          </div>
-        </li>
-        <li>
-          <div className="timeline__content">
-            <HabitCard title="Tidy up bed in the morning" />
-          </div>
-        </li>
-        <li>
-          <div className="timeline__content">
-            <HabitCard title="Tidy up bed in the morning" />
-          </div>
-        </li>
-      </ul>
+    <div className="habits_container">
+      <h1> Your daily habits...</h1>
+      <ol className="habit__list">
+        <HabitCard title="Make bed in the morning." />
+        <HabitCard title="Do 10 pushups." />
+        <HabitCard title="Read a few pages of a book before bed." />
+        <HabitCard title="Do 10 pushups." />
+        <HabitCard title="Make bed in the morning." />
+        <HabitCard title="Do 10 pushups." />
+        <HabitCard title="Read a few pages of a book before bed." />
+        <HabitCard title="Do 10 pushups." />
+      </ol>
     </div>
   );
 };

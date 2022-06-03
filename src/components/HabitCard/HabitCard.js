7c08c330d2694a27a0097bfd892c87c2bc9d986e
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-04 01:33:38 +0530

    👷 ADD : Dialog box for Habit Card

--- src/components/HabitCard/HabitCard.js
+++ src/components/HabitCard/HabitCard.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, CardHeader } from "@progress/kendo-react-layout";
+import { TextArea } from "@progress/kendo-react-labels";
+import { Dialog } from "@progress/kendo-react-dialogs";
 import "./HabitCard.css";
 import { motion } from "framer-motion";
 
 const HabitCard = ({ title }) => {
+  const [expandMe, setExpandMe] = useState(false);
+
+  const expandHabitCard = () => {
+    setExpandMe(!expandMe);
+  };
+
   return (
-    <motion.div whileHover={{ scale: 1.1 }} className="habit__card">
+    <motion.div
+      whileHover={{ scale: 1.1 }}
+      onClick={expandHabitCard}
+      className="habit__card"
+    >
       <Card className="habit__card">
         <CardHeader className="habit__cardheader">{title}</CardHeader>
       </Card>
+
+      {expandMe && (
+        <Dialog title={title} onClose={expandHabitCard}>
+          <h3>Description</h3>
+        </Dialog>
+      )}
     </motion.div>
   );
 };
-
 export default HabitCard;

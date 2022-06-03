3ed50aa0bf9c3c163f3668b3d2fc22d84b1d78c2
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-03 15:57:42 +0530

    👌 IMPROVE: Added content - Home Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,24 @@ import React from "react";
 import "./Home.css";
 import { Button } from "@progress/kendo-react-buttons";
 import { Ripple } from "@progress/kendo-react-ripple";
-
 const Home = () => {
   return (
     <div className="home__container">
       <div className="home__text">
-        <div className="home__text-top"></div>
-        <div className="home__text-bottom"></div>
+        <div className="home__text-top">
+          <div className="top__line-one">
+            <h1>Swaasthaya</h1>
+          </div>
+        </div>
+        <div className="home__text-bottom">
+          <div className="bottom__line-one">
+            <h3>Building habits</h3>
+          </div>
+          <div className="bottom__line-two">
+            <h3 style={{ color: "#FA5D05" }}>Gamified</h3>
+            <h3>!</h3>
+          </div>
+        </div>
       </div>
       <div className="auth__buttons">
         <Ripple>
@@ -24,5 +35,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;

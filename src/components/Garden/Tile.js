aef99fc9cade89dde59098168d7099e7cb182303
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-04 01:54:19 +0530

    👷 ADD : Habit Streaks Page

--- src/components/Garden/Tile.js
+++ src/components/Garden/Tile.js
@@ -1,10 +1,10 @@
 import { motion } from "framer-motion";
 import React from "react";
 import "../Garden/game.css";
-
 const Tile = ({ src, x, y, z }) => {
   return (
     <motion.img
+      whileHover={{ opacity: 0.2 }}
       alt="garden_tile"
       src={src}
       className="tile"
@@ -12,5 +12,4 @@ const Tile = ({ src, x, y, z }) => {
     />
   );
 };
-
 export default Tile;

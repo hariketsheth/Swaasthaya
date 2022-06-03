0a27b6fb4b385ae5d41dc767c8634b164c113420
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-03 18:23:21 +0530

    👌 IMPROVE: SignIn Section - Swaasthaya

--- src/components/SignInModal/SignInModal.js
+++ src/components/SignInModal/SignInModal.js
@@ -1,12 +1,47 @@
+import { Button } from "@progress/kendo-react-buttons";
 import { Dialog } from "@progress/kendo-react-dialogs";
+import { Form, Field, FormElement } from "@progress/kendo-react-form";
 import React from "react";
+import { FormInput } from "../AuthModal/FormInput";
+import { emailValidator, passwordValidator } from "../AuthModal/validators";
 
-const RegisterModal = ({ toggleMethod }) => {
+const SignInModal = ({ toggleMethod }) => {
   return (
-    <Dialog title="Register" onClose={toggleMethod}>
-      <div>Register!</div>
+    <Dialog title="Sign In" onClose={toggleMethod}>
+      <Form
+        onSubmit={() => {}}
+        render={(formRenderProps) => (
+          <FormElement style={{ width: 400 }}>
+            <Field
+              id={"email"}
+              name={"email"}
+              label={"Email"}
+              type={"email"}
+              component={FormInput}
+              validator={emailValidator}
+            />
+            <Field
+              id={"password"}
+              name={"password"}
+              label={"Password"}
+              type={"password"}
+              component={FormInput}
+              validator={passwordValidator}
+            />
+            <div className="k-form-buttons">
+              <Button
+                primary={true}
+                type={"submit"}
+                disabled={!formRenderProps.allowSubmit}
+              >
+                Submit
+              </Button>
+              <Button onClick={formRenderProps.onFormReset}>Clear</Button>
+            </div>
+          </FormElement>
+        )}
+      ></Form>
     </Dialog>
   );
 };
-
-export default RegisterModal;
+export default SignInModal;

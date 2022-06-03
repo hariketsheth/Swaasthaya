164451dc4431e53e7389fa3d5d99d69119ff803b
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-03 21:12:56 +0530

    ✨ UPDATE: Added Validators and buttons

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,73 +1,75 @@
-import React, { useState } from "react";
 import "./Home.css";
 import { Button } from "@progress/kendo-react-buttons";
 import { Ripple } from "@progress/kendo-react-ripple";
-import AuthModal from "../AuthModal/AuthModal";
+import { useHistory } from "react-router";
+import { motion } from "framer-motion";
 
 const Home = () => {
-  const [signInModal, setSignInModal] = useState(false);
-  const [registerModal, setRegisterModal] = useState(false);
-
-  const showSignInModal = () => {
-    setSignInModal(!signInModal);
+  const history = useHistory();
+  const showSignInPage = () => {
+    history.push("/signin");
+  };
+  const showRegisterPage = () => {
+    history.push("/register");
   };
 
-  const showRegisterModal = () => {
-    setRegisterModal(!registerModal);
-  };
   return (
-    <div className="home__container">
-      <div className="home__doodle">
-        <img
-          className="doodle-triangle"
-          src="assets/doodle_triangle.png"
-          alt=""
-        />
-        <img className="doodle" src="assets/homee.png" alt="" />
-      </div>
-      <div className="home__text">
-        <div className="home__text-top">
-          <div className="top__line-one">
-            <h1>Swaasthaya</h1>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="home__container">
+        <div className="home__doodle">
+          <img
+            className="doodle-triangle"
+            src="assets/doodle_triangle.png"
+            alt=""
+          />
+          <img className="doodle" src="assets/homee.png" alt="" />
+        </div>
+        <div className="home__text">
+          <div className="home__text-top">
+            <div className="top__line-one">
+              <h1>Swaasthaya</h1>
+            </div>
+          </div>
+          <div className="home__text-bottom">
+            <div className="bottom__line-one">
+              <h3>Building habits</h3>
+            </div>
+            <div className="bottom__line-two">
+              <h3 style={{ color: "#2856ec" }}>Gamified</h3>
+              <h3>!</h3>
+            </div>
           </div>
         </div>
-        <div className="home__text-bottom">
-          <div className="bottom__line-one">
-            <h3>Building habits</h3>
-          </div>
-          <div className="bottom__line-two">
-            <h3 style={{ color: "#2856ec" }}>Gamified</h3>
-            <h3>!</h3>
-          </div>
+        <div className="auth__buttons">
+          <Ripple>
+            <div className="buttons__one">
+              <Button
+                className="auth-button"
+                primary={true}
+                onClick={showSignInPage}
+              >
+                Sign-In
+              </Button>
+              <Button
+                className="auth-button"
+                primary={true}
+                onClick={showRegisterPage}
+              >
+                Register
+              </Button>
+            </div>
+            <div className="buttons__two">
+              <Button className="login-with-google">Login with Google</Button>
+            </div>
+          </Ripple>
         </div>
       </div>
-      <div className="auth__buttons">
-        <Ripple>
-          <div className="buttons__one">
-            <Button primary={true} onClick={showSignInModal}>
-              Sign-In
-            </Button>
-            <Button primary={true} onClick={showRegisterModal}>
-              Register
-            </Button>
-          </div>
-          <div className="buttons__two">
-            <Button>Login with Google</Button>
-          </div>
-        </Ripple>
-      </div>
-      <div className="signin__modal">
-        {signInModal && (
-          <AuthModal register={false} toggleMethod={showSignInModal} />
-        )}
-      </div>
-
-      <div className="register__modal">
-        {registerModal && (
-          <AuthModal register={true} toggleMethod={showRegisterModal} />
-        )}
-      </div>
-    </div>
+    </motion.div>
   );
 };
+
 export default Home;

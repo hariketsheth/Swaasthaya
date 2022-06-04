788bebfacc99f318fe49bcccf30061cee93bf3c4
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-04 13:48:08 +0530

    👌 IMPROVE: Validators and Auth

--- src/components/AuthModal/AuthModal.js
+++ src/components/AuthModal/AuthModal.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import RegisterModal from '../RegisterModal/RegisterModal';
-import SignInModal from '../SignInModal/SignInModal';
+import React from "react";
+import RegisterModal from "../RegisterModal/RegisterModal";
+import SignInModal from "../SignInModal/SignInModal";
 
 const AuthModal = ({ register, toggleMethod }) => {
-    return (
-        <div className="auth__modal">
-            { register ? <RegisterModal toggleMethod={toggleMethod} /> : <SignInModal toggleMethod={toggleMethod} />}
-        </div>
-    );
-}
+  return (
+    <div className="auth__modal">
+      {register ? (
+        <RegisterModal toggleMethod={toggleMethod} />
+      ) : (
+        <SignInModal toggleMethod={toggleMethod} />
+      )}
+    </div>
+  );
+};
 
 export default AuthModal;

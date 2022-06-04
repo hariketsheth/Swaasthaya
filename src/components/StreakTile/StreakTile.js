386ec2f7027cbe61241bda4565ac35fc69187814
Hariket Sukeshkumar Sheth <SHETHHARIKET@gmail.com>
2022-06-04 14:10:28 +0530

    ✨ UPDATE: StreakTile Added

--- src/components/StreakTile/StreakTile.js
+++ src/components/StreakTile/StreakTile.js
@@ -2,12 +2,16 @@ import { motion } from "framer-motion";
 import React from "react";
 import "./StreakTile.css";
 
-const StreakTile = ({ color }) => {
+const StreakTile = ({ color, h, w }) => {
   return (
     <motion.div
       whileHover={{ scale: 1.1 }}
       className="streak__tile"
-      style={{ backgroundColor: color }}
+      style={{
+        backgroundColor: color,
+        height: h,
+        width: w,
+      }}
     ></motion.div>
   );
 };
